c040c0904a7ceb9c1fd831a6863aad2aa9f94d70
Thibaud Arnault <thyb@getmateria.com>
2020-02-19 18:45:56 +0100

    Migrate @angular/material v9

--- client/stripe.module.ts
+++ client/stripe.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {
-  MatButtonModule,
-  MatRippleModule,
-  MatSnackBarModule,
-  MatCardModule,
-  MatIconModule,
-  MatDialogModule,
-  MatInputModule,
-  MatCheckboxModule,
-  MatRadioModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 
 import { Addon } from '@materia/addons';
